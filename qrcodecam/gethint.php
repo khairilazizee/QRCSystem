4ae6d68ea5b5f54a4eea0be341c24b59b7ced5a1
amirulyus <amirulyus@gmail.com>
2022-12-10 23:07:18 +0800

    update db

--- qrcodecam/gethint.php
+++ qrcodecam/gethint.php
@@ -40,7 +40,7 @@ $a[] = "Vicky";
 $q = $_REQUEST["q"];
 
 //$hint = "";
-$con = mysqli_connect('localhost','root','','qrs');
+$con = mysqli_connect('localhost','root','','qr_convo');
 // Check connection
 if (mysqli_connect_errno())
 {
